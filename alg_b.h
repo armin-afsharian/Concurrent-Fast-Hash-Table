e3ab85b6d51f7504eec9791d098c981f98b5e95c
armin-afsharian <armin8545@gmail.com>
2024-02-15 16:18:43 -0500

    Slightly improve Lock-based hash table with algorithm B

--- alg_b.h
+++ alg_b.h
@@ -4,7 +4,7 @@
 #include <mutex>
 using namespace std;
 
-class AlgorithmA {
+class AlgorithmB {
 public:
     static constexpr int TOMBSTONE = -1;
     static constexpr int NULL_VALUE = -2;
@@ -22,14 +22,13 @@ public:
     
     padded_bucket * data;
 
-    AlgorithmA(const int _numThreads, const int _capacity);
-    ~AlgorithmA();
+    AlgorithmB(const int _numThreads, const int _capacity);
+    ~AlgorithmB();
     bool insertIfAbsent(const int tid, const int & key);
     bool erase(const int tid, const int & key);
     long getSumOfKeys();
     void printDebuggingDetails(); 
 };
-
 
 /**
  * constructor: initialize the hash table's internals
@@ -37,7 +36,7 @@ public:
  * @param _numThreads maximum number of threads that will ever use the hash table (i.e., at least tid+1, where tid is the largest thread ID passed to any function of this class)
  * @param _capacity is the INITIAL size of the hash table (maximum number of elements it can contain WITHOUT expansion)
  */
-AlgorithmA::AlgorithmA(const int _numThreads, const int _capacity)
+AlgorithmB::AlgorithmB(const int _numThreads, const int _capacity)
 : numThreads(_numThreads), capacity(_capacity) {
     data = new padded_bucket[capacity];
     for (int i = 0; i < capacity; i++)
@@ -45,52 +44,59 @@ AlgorithmA::AlgorithmA(const int _numThreads, const int _capacity)
 }
 
 // destructor: clean up any allocated memory, etc.
-AlgorithmA::~AlgorithmA() {
+AlgorithmB::~AlgorithmB() {
     delete[] data;
 }
 
 // semantics: try to insert key. return true if successful (if key doesn't already exist), and false otherwise
-bool AlgorithmA::insertIfAbsent(const int tid, const int & key) {
+bool AlgorithmB::insertIfAbsent(const int tid, const int & key) {
     u_int32_t h = murmur3(key);
     for(int i = 0; i < capacity; i++) {
         int index = (h + i) % capacity;
-        data[index].m.lock();
         int found = data[index].key;
-        if(found == key) {
+        if (found == NULL_VALUE) {
+            data[index].m.lock();
+            found = data[index].key;
+            if(found == NULL_VALUE) {
+                data[index].key = key;
+                data[index].m.unlock();
+                return true;
+            } else if(found == key) {
+                data[index].m.unlock();
+                return false;
+            }
             data[index].m.unlock();
+        } else if(found == key) {
             return false;
-        } else if (found == NULL_VALUE) {
-            data[index].key = key;
-            data[index].m.unlock();
-            return true;
-        }
-        data[index].m.unlock();
+        } 
     }
     return false;
 }
 
 // semantics: try to erase key. return true if successful, and false otherwise
-bool AlgorithmA::erase(const int tid, const int & key) {
+bool AlgorithmB::erase(const int tid, const int & key) {
     u_int32_t h = murmur3(key);
     for(int i = 0; i < capacity; i++) {
         int index = (h + i) % capacity;
-        data[index].m.lock();
         int found = data[index].key;
-        if(found == NULL_VALUE) {
+        if(found == key) {
+            data[index].m.lock();
+            found = data[index].key;
+            if(found == key) {
+                data[index].key = TOMBSTONE;
+                data[index].m.unlock();
+                return true;
+            }
             data[index].m.unlock();
+        } else if(found == NULL_VALUE) {
             return false;
-        } else if(found == key) {
-            data[index].key = TOMBSTONE;
-            data[index].m.unlock();
-            return true;
         }
-        data[index].m.unlock();
     }
     return false;
 }
 
 // semantics: return the sum of all KEYS in the set
-int64_t AlgorithmA::getSumOfKeys() {
+int64_t AlgorithmB::getSumOfKeys() {
     int64_t sum = 0;
     for(int i = 0; i < capacity; i++) {
         int key = data[i].key;
@@ -101,6 +107,6 @@ int64_t AlgorithmA::getSumOfKeys() {
 }
 
 // print any debugging details you want at the end of a trial in this function
-void AlgorithmA::printDebuggingDetails() {
+void AlgorithmB::printDebuggingDetails() {
     
 }
