fdde1b43b32d2405948ec666efe6abc9760b8ebf
armin-afsharian <armin8545@gmail.com>
2024-02-16 10:58:57 -0500

    Lock-free probing hase table completed

--- alg_c.h
+++ alg_c.h
@@ -1,10 +1,9 @@
 #pragma once
 #include "util.h"
 #include <atomic>
-#include <mutex>
 using namespace std;
 
-class AlgorithmB {
+class AlgorithmC {
 public:
     static constexpr int TOMBSTONE = -1;
     static constexpr int NULL_VALUE = -2;
@@ -15,15 +14,14 @@ public:
     char padding2[PADDING_BYTES];
 
     struct padded_bucket {
-        mutex m;
-        int key;
-        char padding[64 - (sizeof(int) + sizeof(mutex))];
+        atomic<int> key;
+        char padding[64 - (sizeof(atomic<int>))];
     };
-    
+
     padded_bucket * data;
 
-    AlgorithmB(const int _numThreads, const int _capacity);
-    ~AlgorithmB();
+    AlgorithmC(const int _numThreads, const int _capacity);
+    ~AlgorithmC();
     bool insertIfAbsent(const int tid, const int & key);
     bool erase(const int tid, const int & key);
     long getSumOfKeys();
@@ -36,68 +34,56 @@ public:
  * @param _numThreads maximum number of threads that will ever use the hash table (i.e., at least tid+1, where tid is the largest thread ID passed to any function of this class)
  * @param _capacity is the INITIAL size of the hash table (maximum number of elements it can contain WITHOUT expansion)
  */
-AlgorithmB::AlgorithmB(const int _numThreads, const int _capacity)
+AlgorithmC::AlgorithmC(const int _numThreads, const int _capacity)
 : numThreads(_numThreads), capacity(_capacity) {
     data = new padded_bucket[capacity];
-    for (int i = 0; i < capacity; i++)
+    for(int i = 0; i < capacity; i++)
         data[i].key = NULL_VALUE;
 }
 
 // destructor: clean up any allocated memory, etc.
-AlgorithmB::~AlgorithmB() {
+AlgorithmC::~AlgorithmC() {
     delete[] data;
 }
 
 // semantics: try to insert key. return true if successful (if key doesn't already exist), and false otherwise
-bool AlgorithmB::insertIfAbsent(const int tid, const int & key) {
-    u_int32_t h = murmur3(key);
-    for(int i = 0; i < capacity; i++) {
-        int index = (h + i) % capacity;
-        int found = data[index].key;
-        if (found == NULL_VALUE) {
-            data[index].m.lock();
-            found = data[index].key;
-            if(found == NULL_VALUE) {
-                data[index].key = key;
-                data[index].m.unlock();
-                return true;
-            } else if(found == key) {
-                data[index].m.unlock();
-                return false;
-            }
-            data[index].m.unlock();
-        } else if(found == key) {
-            return false;
-        } 
-    }
-    return false;
-}
-
-// semantics: try to erase key. return true if successful, and false otherwise
-bool AlgorithmB::erase(const int tid, const int & key) {
+bool AlgorithmC::insertIfAbsent(const int tid, const int & key) {
     u_int32_t h = murmur3(key);
     for(int i = 0; i < capacity; i++) {
         int index = (h + i) % capacity;
         int found = data[index].key;
         if(found == key) {
-            data[index].m.lock();
-            found = data[index].key;
-            if(found == key) {
-                data[index].key = TOMBSTONE;
-                data[index].m.unlock();
-                return true;
-            }
-            data[index].m.unlock();
             return false;
         } else if(found == NULL_VALUE) {
+            int expected = NULL_VALUE;
+            if(data[index].key.compare_exchange_strong(expected, key)) {
+                return true;
+            } if(data[index].key == key) {
+                return false;
+            }
+        }
+    }
+    return false;
+}
+
+// semantics: try to erase key. return true if successful, and false otherwise
+bool AlgorithmC::erase(const int tid, const int & key) {
+    u_int32_t h = murmur3(key);
+    for(int i = 0; i < capacity; i++) {
+        int index = (h + i) % capacity;
+        int found = data[index].key;
+        if(found == NULL_VALUE) {
             return false;
+        } else if(found == key) {
+            int expected = key;
+            return data[index].key.compare_exchange_strong(expected, TOMBSTONE);
         }
     }
     return false;
 }
 
 // semantics: return the sum of all KEYS in the set
-int64_t AlgorithmB::getSumOfKeys() {
+int64_t AlgorithmC::getSumOfKeys() {
     int64_t sum = 0;
     for(int i = 0; i < capacity; i++) {
         int key = data[i].key;
@@ -108,6 +94,6 @@ int64_t AlgorithmB::getSumOfKeys() {
 }
 
 // print any debugging details you want at the end of a trial in this function
-void AlgorithmB::printDebuggingDetails() {
+void AlgorithmC::printDebuggingDetails() {
     
 }
