990c8202dfb653b30f8d5cd6745daafbfbebb5f8
armin-afsharian <armin8545@gmail.com>
2024-02-19 19:03:21 -0500

    Expandable hash table completed!

--- alg_d.h
+++ alg_d.h
@@ -9,8 +9,12 @@ private:
     enum {
         MARKED_MASK = (int) 0x80000000,     // most significant bit of a 32-bit key
         TOMBSTONE = (int) 0x7FFFFFFF,       // largest value that doesn't use bit MARKED_MASK
-        EMPTY = (int) 0
+        EMPTY = (int) 0,
+        MAXIMUM_HASH = (uint32_t)0xFFFFFFFF
     }; // with these definitions, the largest "real" key we allow in the table is 0x7FFFFFFE, and the smallest is 1 !!
+
+    static constexpr int CHUNK_SIZE = 4096;
+    static constexpr int CAPCITY_INCREASE = 4;
 
     struct table {
         char padding0[64];
@@ -20,27 +24,32 @@ private:
         int oldCapacity;
         int numThreads;
         counter * approxCounter;
-        counter * accurateCounter;
+        counter * deleteCounter;
         atomic<int> chuncksClaimed;
         atomic<int> chuncksDone;
-        char padding1[64];
-
         table(const int _capacity, const int _numThreads) 
         : capacity(_capacity), numThreads(_numThreads), old(NULL), oldCapacity(0), chuncksClaimed(0), chuncksDone(0) {
             data = new atomic<int>[capacity];
             for(int i = 0; i < capacity; i++)
-                data[i] = EMPTY;
+                data[i].store(EMPTY, memory_order_relaxed);
             approxCounter = new counter(_numThreads);
-            accurateCounter = new counter(_numThreads);
+            deleteCounter = new counter(_numThreads);
         }
 
         table(table * t) {
             old = t->data;
             oldCapacity = t->capacity;
-            capacity = 4 * t->capacity;
+            int insertCount = t->approxCounter->get();
+            int deleteCount = t->deleteCounter->get();
+            int numOfKeys = insertCount - deleteCount; // number of keys in the table;
+            if (numOfKeys > 0)
+                capacity = numOfKeys * CAPCITY_INCREASE;
+            else
+                capacity = oldCapacity * CAPCITY_INCREASE;
+                
             numThreads = t->numThreads;
             approxCounter = new counter(numThreads);
-            accurateCounter = new counter(numThreads);
+            deleteCounter = new counter(numThreads);
             chuncksClaimed.store(0, memory_order_relaxed);
             chuncksDone.store(0, memory_order_relaxed);
             data = new atomic<int>[capacity];
@@ -48,10 +57,25 @@ private:
                 data[i].store(EMPTY, memory_order_relaxed);
         }
 
-        ~table() {
-            delete[] data, old;
-            delete approxCounter, accurateCounter;
+        void print(int k) {
+            for (int i = 0; i < k; i++) {
+                int temp = data[i];
+                if (temp == TOMBSTONE)
+                    cout << "O";
+                else if (temp == EMPTY)
+                    cout << ".";
+                else
+                    cout << 'X';
+            }
+            cout << "END\n *** \n *** \n";
         }
+
+        ~table() {
+            if(data)
+                delete[] data;
+            delete approxCounter, deleteCounter;
+        }
+
     };
     
     bool expandAsNeeded(const int tid, table * t, int i);
@@ -71,6 +95,7 @@ public:
     ~AlgorithmD();
     bool insertIfAbsent(const int tid, const int & key, bool disableExpansion);
     bool erase(const int tid, const int & key);
+    bool insertForMigration(const int tid, const int & key);
     long getSumOfKeys();
     void printDebuggingDetails(); 
 };
@@ -88,13 +113,22 @@ AlgorithmD::AlgorithmD(const int _numThreads, const int _capacity)
 
 // destructor: clean up any allocated memory, etc.
 AlgorithmD::~AlgorithmD() {
-    delete currentTable;
+    table * t = currentTable;
+    if(t) {
+        if(t->old)
+            delete[] currentTable.load()->old;
+        if(t->approxCounter)
+            delete currentTable.load()->approxCounter;
+        if(t->deleteCounter)
+            delete currentTable.load()->deleteCounter;
+        delete t;
+    }
 }
 
 bool AlgorithmD::expandAsNeeded(const int tid, table * t, int i) {
     helpExpansion(tid, t);
-    if((t->approxCounter->get() > t->capacity/2) ||
-        (i > 10 && t->accurateCounter->getAccurate() > t->capacity/2)) {
+    if(((t->approxCounter->get()) > (0.5 * t->capacity)) ||
+        ((i > 100) && ((t->approxCounter->getAccurate()) > t->capacity/2))) {
             startExpansion(tid, t);
             return true;
     }
@@ -102,7 +136,7 @@ bool AlgorithmD::expandAsNeeded(const int tid, table * t, int i) {
 }
 
 void AlgorithmD::helpExpansion(const int tid, table * t) {
-    int totalOldChunks = ceil(t->oldCapacity / 4096);
+    int totalOldChunks = ceil(t->oldCapacity / (double) CHUNK_SIZE);
     while(t->chuncksClaimed < totalOldChunks) {
         int myChunk = t->chuncksClaimed.fetch_add(1);
         if(myChunk < totalOldChunks) {
@@ -110,7 +144,7 @@ void AlgorithmD::helpExpansion(const int tid, table * t) {
             t->chuncksDone.fetch_add(1);
         }
     }
-    while(!(t->chuncksDone == totalOldChunks));
+    while(t->chuncksDone < totalOldChunks);
 }
 
 void AlgorithmD::startExpansion(const int tid, table * t) {
@@ -118,22 +152,67 @@ void AlgorithmD::startExpansion(const int tid, table * t) {
         table * t_new = new table(t);
         if(!currentTable.compare_exchange_strong(t, t_new))
             delete t_new;
+        else {
+            if(t->old)
+                delete[] t->old;
+        }
     }
     helpExpansion(tid, currentTable);
 }
 
 void AlgorithmD::migrate(const int tid, table * t, int myChunk) {
+    int start_index = myChunk * CHUNK_SIZE;
+    int end_index = min((myChunk + 1) * CHUNK_SIZE, t->oldCapacity);
+    for(int i = start_index; i < end_index; i++) {
+        int key = t->old[i];
+        if(key == TOMBSTONE)
+            continue;
+        else {
+            while(!t->old[i].compare_exchange_strong(key, key | MARKED_MASK)) {
+                key = t->old[i];
+                if(key == TOMBSTONE)
+                    break;
+            }
+            if((key != TOMBSTONE) && (key != EMPTY)) {
+                insertForMigration(tid, key);
+            }
+                
+        }
+    }
+}
 
+bool AlgorithmD::insertForMigration(const int tid, const int & key) {
+    table * t = currentTable;
+    double ii = murmur3(key);
+    uint32_t h = floor(ii / MAXIMUM_HASH * (uint32_t)t->capacity);
+    for(int i = 0; i < t->capacity; i++) {
+        int index = (h + i) % t->capacity;
+        int found = t->data[index].load(memory_order_relaxed);
+        if(found == key)
+            return false;
+        else if(found == EMPTY) {
+            int expected = EMPTY;
+            if(t->data[index].compare_exchange_strong(expected, key)) {
+                t->approxCounter->inc(tid);
+                return true;
+            }
+            found = t->data[index];
+            if(found == key)
+                return false;
+        }
+    }
+        
+    return false;
 }
 
 // semantics: try to insert key. return true if successful (if key doesn't already exist), and false otherwise
 bool AlgorithmD::insertIfAbsent(const int tid, const int & key, bool disableExpansion = false) {
     table * t = currentTable;
-    u_int32_t h = murmur3(key);
+    double ii = murmur3(key);
+    uint32_t h = floor(ii / MAXIMUM_HASH * (uint32_t)t->capacity);
     for(int i = 0; i < t->capacity; i++) {
-        if(disableExpansion && expandAsNeeded(tid, t, i)) {
+        if(!disableExpansion && expandAsNeeded(tid, t, i))
             return insertIfAbsent(tid, key);
-        }
         int index = (h + i) % t->capacity;
         int found = t->data[index];
         if(found & MARKED_MASK)
@@ -143,7 +222,6 @@ bool AlgorithmD::insertIfAbsent(const int tid, const int & key, bool disableExpa
         else if(found == EMPTY) {
             int expected = EMPTY;
             if(t->data[index].compare_exchange_strong(expected, key)) {
-                t->accurateCounter->inc(tid);
                 t->approxCounter->inc(tid);
                 return true;
             }
@@ -161,8 +239,10 @@ bool AlgorithmD::insertIfAbsent(const int tid, const int & key, bool disableExpa
 // semantics: try to erase key. return true if successful, and false otherwise
 bool AlgorithmD::erase(const int tid, const int & key) {
     table * t = currentTable;
-    u_int32_t h = murmur3(key);
+    double ii = murmur3(key);
+    uint32_t h = floor(ii / MAXIMUM_HASH * (uint32_t)t->capacity);
     for(int i = 0; i < t->capacity; i++) {
+        helpExpansion(tid, t);
         int index = (h + i) % t->capacity;
         int found = t->data[index];
         if(found & MARKED_MASK)
@@ -171,8 +251,10 @@ bool AlgorithmD::erase(const int tid, const int & key) {
             return false;
         else if(found == key) {
             int expected = key;
-            if(t->data[index].compare_exchange_strong(expected, TOMBSTONE))
+            if(t->data[index].compare_exchange_strong(expected, TOMBSTONE)) {
+                t->deleteCounter->inc(tid);
                 return true;
+            }
             int found = t->data[index];
             if(found & MARKED_MASK)
                 return erase(tid, key);
